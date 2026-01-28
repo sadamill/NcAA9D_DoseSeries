7cb11ea6b9c106d848da910728a58b0b0ea677cf
sadamill <samill22@ncsu.edu>
2026-01-28 11:06:41 -0500

    oops new data structure too

--- scripts/dose_analysis.R
+++ scripts/dose_analysis.R
@@ -1,7 +1,7 @@
 # Data import -------------------------------------------------------------
 
 wedgeDWDs <- lapply(1:38, function(i) {
-  read.csv(paste0('input/wedge_', i, '.csv'), header = TRUE) %>% 
+  read.csv(paste0('input/raddose/wedge_', i, '.csv'), header = TRUE) %>% 
     dplyr::mutate(number = paste(!!i), datatype = 'Wedge')
 }) # Import all wedge DWD traces for the wedges (for wedge and pseudohelix DWD calculations)
 
@@ -12,7 +12,7 @@ dwds <- tibble::tibble(
 ) # Initialize a data frame to put all the doses in
 
 dwds$wedges <- sapply(2:37, function(wedgeNumber) {
-  wedge <- allData$wedgeDWDs[[wedgeNumber]]
+  wedge <- wedgeDWDs[[wedgeNumber]]
   mean(wedge$DWD)
 }) # Wedges 2-37 were used, so calculate the average DWD for all these
 
@@ -21,7 +21,7 @@ dwds$wedges <- sapply(2:37, function(wedgeNumber) {
 # and average these across wedges 2-37 to calculate the average DWD for a pseudhelix
 dwds$pseudohelices <- sapply(2:37, function(pseudohelixNumber) {
   subwedgeAverages <- sapply(2:37, function(subwedgeNumber) {
-    wedge <- allData$wedgeDWDs[[subwedgeNumber]]
+    wedge <- wedgeDWDs[[subwedgeNumber]]
     startAngle <- ((pseudohelixNumber - 1) * 5) + ((subwedgeNumber - 2) * 5) # Start angle depends on both the pseudohelix number and subwedge you are calculating
     subwedgeAverage <- dplyr::filter(wedge, DWD.Angle >= startAngle & DWD.Angle <= startAngle + 4)$DWD %>% # Extract the DWD column for angles within a target subwedge (5 frames = 4° angular range) 
       mean() # Average the extracted DWDs
