b6f9468e2077cb69e9c1785c3171cc0547fee34f
sadamill <samill22@ncsu.edu>
2026-02-06 11:28:33 -0500

    more restructuring

--- 2-decay_parameter_estimation/2.1-process_with_dials.sh
+++ 2-decay_parameter_estimation/2.1-process_with_dials.sh
@@ -3,12 +3,12 @@
 # directory setup
 for i in {1..36}
 do
-	mkdir -p $(printf "2-decay_parameter_estimation/output/wedge%02d" "$i")
+	mkdir -p $(printf "2-decay_parameter_estimation/output/dials_output/wedge%02d" "$i")
 done
 
 for i in {1..25}
 do
-	mkdir -p $(printf "2-decay_parameter_estimation/output/start_frame_%02d/subwedges" "$i")
+	mkdir -p $(printf "2-decay_parameter_estimation/output/dials_output/start_frame_%02d/subwedges" "$i")
 done
 
 # import and bin images
@@ -16,8 +16,8 @@ for i in {2..37}
 do
     (
 		new_wedge_num=$((i-1))
-	    cd $(printf "2-decay_parameter_estimation/output/wedge%02d" "$new_wedge_num")
-	    dials.import template=$(printf "../../../0-diffraction_images/wedge%02d/HR00498_Pn5_wedge_%02d.####.gz" "$i" "$i")
+	    cd $(printf "2-decay_parameter_estimation/output/dials_output/wedge%02d" "$new_wedge_num")
+	    dials.import template=$(printf "../../../../0-diffraction_images/wedge%02d/HR00498_Pn5_wedge_%02d.####.gz" "$i" "$i")
 	    dials.find_spots imported.expt
 	)
 done
@@ -25,7 +25,7 @@ done
 for start_frame in {1..25}
 do
     (
-		cd $(printf "2-decay_parameter_estimation/output/start_frame_%02d" "$start_frame")
+		cd $(printf "2-decay_parameter_estimation/output/dials_output/start_frame_%02d" "$start_frame")
 	    end_frame=$((${start_frame}+4))
 	    for wedge in {1..36}
 	    do
