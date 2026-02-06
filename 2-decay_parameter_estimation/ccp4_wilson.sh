224c5b34e64bd4c8283b114fb28c7c45a6828bf1
sadamill <samill22@ncsu.edu>
2026-02-06 11:00:26 -0500

    renumbering

---
+++ 2-decay_parameter_estimation/ccp4_wilson.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for start_frame in {1..25}
+do
+    cd $(printf "start_frame_%02d" "$start_frame")
+	wilson hklin integrated.mtz <<-EOF > ccp4_wilson.txt
+	nresidues 892
+	resolution 68.72 1.1
+	labin IP=I SIGIP=SIGI
+	title wilson.log
+	EOF
+    cd ..
+done
+
+grep "Least squares" start_frame_*/ccp4_wilson.txt | sed -r "s/(^.*B  =  |        SCALE.*$)//g" > ccp4_wilson_b.csv
+grep "Least squares" start_frame_*/ccp4_wilson.txt | sed -r "s/(^.*SCALE  =  )//g" > ccp4_wilson_scale.csv
--- 2-decay_parameter_estimation/process_estimate.sh
+++ 2-decay_parameter_estimation/process_estimate.sh
@@ -3,12 +3,12 @@
 # directory setup
 for i in {1..36}
 do
-	mkdir -p $(printf "1-decay_parameter_estimation/output/wedge%02d" "$i")
+	mkdir -p $(printf "2-decay_parameter_estimation/output/wedge%02d" "$i")
 done
 
 for i in {1..25}
 do
-	mkdir -p $(printf "1-decay_parameter_estimation/output/start_frame_%02d/subwedges" "$i")
+	mkdir -p $(printf "2-decay_parameter_estimation/output/start_frame_%02d/subwedges" "$i")
 done
 
 # import and bin images
@@ -16,7 +16,7 @@ for i in {2..37}
 do
     (
 		new_wedge_num=$((i-1))
-	    cd $(printf "1-decay_parameter_estimation/output/wedge%02d" "$new_wedge_num")
+	    cd $(printf "2-decay_parameter_estimation/output/wedge%02d" "$new_wedge_num")
 	    dials.import template=$(printf "../../../0-diffraction_images/wedge%02d/HR00498_Pn5_wedge_%02d.####.gz" "$i" "$i")
 	    dials.find_spots imported.expt
 	)
@@ -25,7 +25,7 @@ done
 for start_frame in {1..25}
 do
     (
-		cd $(printf "1-decay_parameter_estimation/output/start_frame_%02d" "$start_frame")
+		cd $(printf "2-decay_parameter_estimation/output/start_frame_%02d" "$start_frame")
 	    end_frame=$((${start_frame}+4))
 	    for wedge in {1..36}
 	    do
@@ -38,6 +38,6 @@ do
 	    dials.index combined.expt combined.refl space_group=P1211
 	    dials.refine indexed.expt indexed.refl
 	    dials.integrate refined.expt refined.refl
-		dials.export integrated.*
+		dials.export integrated.expt refined.refl
 	)
 done
