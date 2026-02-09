0920ee76d36e45803d580437caa528af75123c8c
sadamill <samill22@ncsu.edu>
2026-02-09 13:19:55 -0500

    add actual filename

--- 3-ddwd_calculation/3.1-directory_setup.sh
+++ 3-ddwd_calculation/3.1-directory_setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# directory setup
+mkdir  3-ddwd_calculation/output/r_output
+
 # copy input files from previous step
-for i in {1..36}
-do
-	cp 2-decay_parameter_estimation/output/r_output/*___________.csv 3-decay_parameter_estimation/input/raddose_input/
-done
+cp 2-decay_parameter_estimation/output/r_output/decay_params.csv 3-ddwd_calculation/input/raddose_input/
+cp 1-fwd_calculation/output/r_output/fwds.csv 3-ddwd_calculation/input/r_input
