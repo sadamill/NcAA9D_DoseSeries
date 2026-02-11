1d7a860a4283f6b43bbd8227463fba730735b099
sadamill <samill22@ncsu.edu>
2026-02-11 13:21:07 -0500

    change for structure refinement additions

--- 6-structure_refinements/6.1-directory_setup.sh
+++ 6-structure_refinements/6.1-directory_setup.sh
@@ -12,6 +12,6 @@ done
 # copy output files from previous step
 for i in {1..36}
 do
-	cp $(printf "5-data_processing/output/pseudohelix${i}/merged.mtz") 6-structure_refinements/input/starting_maps/pseudohelix${i}_merged.mtz
-	cp $(printf "5-data_processing/output/wedge${i}/merged.mtz") 6-structure_refinements/input/starting_maps/wedge${i}_merged.mtz
+	cp $(printf "5-data_processing/output/pseudohelix%02d/merged.mtz" "$i") 6-structure_refinements/input/starting_maps/pseudohelix${i}_merged.mtz
+	cp $(printf "5-data_processing/output/wedge%02d/merged.mtz" "$i") 6-structure_refinements/input/starting_maps/wedge${i}_merged.mtz
 done
