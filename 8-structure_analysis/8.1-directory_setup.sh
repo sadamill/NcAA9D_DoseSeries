71286dcad24d5530e7f61f88e74e6407423e673b
sadamill <samill22@ncsu.edu>
2026-02-16 13:06:04 -0500

    make new folders

--- 8-structure_analysis/8.1-directory_setup.sh
+++ 8-structure_analysis/8.1-directory_setup.sh
@@ -8,3 +8,5 @@ do
 done
 
 cp 7-structure_statistics/output/* 8-structure_analysis/input/crystal_statistics/
+cp 4-sampling/output/samples.csv 8-structure_analysis/input/
+cp 3-ddwd_calculation/output/r_output/ddwds.csv 8-structure_analysis/input
