c0b872d2e700b5bde31a1d366b941298f3424aac
sadamill <samill22@ncsu.edu>
2026-02-24 08:50:45 -0500

    udpate

--- additional_scripts/clustering_test.R
+++ additional_scripts/clustering_test.R
@@ -1,22 +1,22 @@
 library(factoextra)
 library(cowplot)
 
-wide_wedges <- wedgeAtoms |> purrr::map(\(pdb) {
+wide_wedges <- pseudohelixAtoms |> purrr::map(\(pdb) {
   pdb |> as_tibble() |> 
+    filter(!elesy == "H") |> # filter out hydrogens
     select(c(eleno, x:b)) |> 
     pivot_wider(
       names_from = c(eleno),
-      values_from = c(x, y, z, b, o)
+      values_from = c(x, y, z, o, b)
     )
 }) |> purrr::list_rbind() |> 
   dplyr::select(where(~ var(.) > 0))
 
-pca <- prcomp(wide_wedges, scale = TRUE)
+pca <- prcomp(wide_wedges, scale. = TRUE)
 factoextra::fviz_eig(pca, addlabels = TRUE, ncp = 36)
 factoextra::fviz_cos2(pca, choice = "var", axes = 1:4)
 
 pca_filtered <- pca$x[,1:2] |> dplyr::as_tibble()
-
 
 cowplot::plot_grid(
   factoextra::fviz_nbclust(pca_filtered, kmeans, "gap_stat"),
@@ -24,11 +24,20 @@ cowplot::plot_grid(
   factoextra::fviz_nbclust(pca_filtered, kmeans, "wss"),
   ncol = 2
 )
-  
-factoextra::eclust(pca_filtered, FUNcluster = "kmeans", k = 3)
+
+cluster <- factoextra::eclust(pca_filtered, FUNcluster = "kmeans", k = 3)
 
 pca_filtered |> 
   bind_cols(number = 1:36) |> 
   ggplot(aes(x = PC1, y = PC2, color = number, label = number)) +
   geom_path() +
   geom_text()
+
+pca_filtered |> 
+  bind_cols(number = 1:36) |> 
+  ggplot(aes(x = number, y = PC1)) +
+  geom_path()
+  
+pca$rotation |> as_tibble(rownames = "rowname") |> select(rowname:PC1) |> arrange(desc(abs(PC1))) |> view()
+
+factoextra::fviz(pca, element = "ind")
