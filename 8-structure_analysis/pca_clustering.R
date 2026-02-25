e850450c53432524a357669d47ce52566bc149c3
sadamill <samill22@ncsu.edu>
2026-02-25 17:09:04 -0500

    better pca plots

--- 8-structure_analysis/pca_clustering.R
+++ 8-structure_analysis/pca_clustering.R
@@ -84,8 +84,8 @@ ggplots$Light$pca$broken_stick <- cowplot::plot_grid(bs_wedge$plot, bs_pseudohel
 pca_wedge_filtered <- pca_wedge$x[,1:bs_wedge$ndim] |> dplyr::as_tibble()
 pca_pseudohelix_filtered <- pca_pseudohelix$x[,1:bs_pseudohelix$ndim] |> dplyr::as_tibble()
 
-wedge_cluster_choice <- NbClust::NbClust(pca_wedge_filtered, method = "kmeans")
-pseudohelix_cluster_choice <- NbClust::NbClust(pca_pseudohelix_filtered, method = "kmeans")
+wedge_cluster_choice <- NbClust::NbClust(pca_wedge_filtered, method = "kmeans", index = "alllong")
+pseudohelix_cluster_choice <- NbClust::NbClust(pca_pseudohelix_filtered, method = "kmeans", index = "alllong")
 
 wedge_clusters <- bind_cols(dataset_number = 1:36,
                             cluster = factor(wedge_cluster_choice$Best.partition),
@@ -95,22 +95,63 @@ pseudohelix_clusters <- bind_cols(dataset_number = 1:36,
                             pca_pseudohelix_filtered)
 
 plot_clusters <- function(data) {
+  outline_pts <- tibble(
+    x = c(0,250,250,0,0,
+          0,-50,-50,0,
+          -50,200,250)/33,
+    y = c(0,0,1188,1188,0,
+          0,50,1238,1188,
+          1238,1238,1188)/33+0.5,
+    id = c(1,1,1,1,1,
+           2,2,2,2,
+           3,3,3)/33
+  )
   
-  long_p <- ggplot(data, aes(y = dataset_number, x = 1, fill = cluster)) +
-    geom_tile() +
-    paletteer::scale_fill_paletteer_d(palette = "IslamicArt::samarqand2") +
+  deltas <- tibble(
+    x = c(250, rep(c(0,-250,-50,0,50,250), 35), 
+          0,-50,-250,0,50),
+    y = c(0, rep(c(33,0,50,-33,-50,33), 35), 
+          33,50,0,-33,-50)
+  )
+  
+  cluster_vec <- data$cluster
+  
+  polygon_pts <- purrr::accumulate(
+    seq(1, nrow(deltas)), 
+    \(acc_deltas, idx) {
+      
+      delta_next <- deltas[idx,]
+      x <- acc_deltas$x + delta_next$x
+      y <- acc_deltas$y + delta_next$y
+      
+      point <- tibble(x = x, y = y)
+      return(point)
+    },
+    .init = tibble(x = 0, y = 0)
+  )[-1,] |> 
+    bind_cols(id = sort(rep(1:36, 6)), 
+              cluster = {purrr::map(cluster_vec, \(x) rep(x, 6)) |> list_c()}) |> 
+    mutate(x = x/33,
+           y = y/33+0.5)
+  
+  palette <- "PNWColors::Starfish"
+  long_p <- ggplot(polygon_pts, aes(x = x, y = y, fill = cluster, color = cluster, group = id)) +
+    geom_polygon() +
+    geom_path(data = outline_pts, aes(x = x, y = y), inherit.aes = FALSE) +
+    scale_fill_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +  
+    scale_color_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +  
     coord_cartesian(expand = FALSE) +
     scale_x_continuous(breaks = NULL) +
     scale_y_continuous(breaks = seq(1, 36, 2), position = "right") +
     labs(x = NULL, y = "Dataset Number") +
     ggtheme_light() +
-    theme(legend.position = "none", panel.grid = element_blank())
+    theme(legend.position = "none", panel.grid = element_blank(), panel.border = element_blank())
   envelope_p <- data |> 
     ggplot(aes(x = PC1, y = PC2, color = cluster, fill = cluster)) +
     geom_point(show.legend = FALSE) +
-    ggforce::geom_mark_hull(expand = 0, radius = 0) +
-    paletteer::scale_color_paletteer_d(palette = "IslamicArt::samarqand2") +
-    paletteer::scale_fill_paletteer_d(palette = "IslamicArt::samarqand2") +
+    ggforce::geom_mark_hull(expand = 0.01, radius = 0.01) +
+    scale_fill_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +  
+    scale_color_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +   
     ggtheme_light() +
     theme(legend.position = "none")
   legend <- get_legend(
@@ -118,10 +159,10 @@ plot_clusters <- function(data) {
       ggplot(aes(x = PC1, y = PC2, color = cluster, fill = cluster)) +
       geom_point(show.legend = FALSE) +
       ggforce::geom_mark_hull(expand = 0, radius = 0) +
-      paletteer::scale_color_paletteer_d(palette = "IslamicArt::samarqand2") +
-      paletteer::scale_fill_paletteer_d(palette = "IslamicArt::samarqand2") +
+      scale_fill_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +  
+      scale_color_manual(labels = 1:3, breaks = 1:3, values = c("#01608c", "#9462ff", "#ee8cab")) +  
       labs(fill = "Cluster", color = "Cluster") +
-      theme(legend.position = "right", legend.key.height = unit(4, "mm"))
+      theme(legend.position = "right", legend.key.height = unit(4, "mm"), rect = element_blank())
   )
   
   right_col <- plot_grid(legend, long_p, ncol = 1, rel_heights = c(0.25,1))
