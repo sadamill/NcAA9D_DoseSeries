afdd56c9c768c5fe0d25aecb89bdf6b9f65d7d9a
sadamill <samill22@ncsu.edu>
2026-03-03 15:02:54 -0500

    nls and new plots

--- 2-decay_parameter_estimation/2.4-estimate_gamma_b0_beta.R
+++ 2-decay_parameter_estimation/2.4-estimate_gamma_b0_beta.R
@@ -1,5 +1,6 @@
 # calculate γ, β, and B0 for RADDOSE input
 
+library(propagate)
 library(tidyverse)
 library(cowplot)
 
@@ -21,56 +22,54 @@ scaling_params <- dplyr::bind_cols(
   )
 
 wilson_b_fit <- lm(wilson_b ~ fwd, scaling_params, subset = 1:9)
-scale_fit <- lm(log(k) ~ {fwd^2}, scaling_params, subset = 1:17)$coefficients |> 
-  setNames(c("c", "g"))
-scale_fit[1] <- exp(scale_fit[1])
-scale_fit[2] <- sqrt(-scale_fit[2])
+scale_fit <- nls(k ~ c * exp(-(fwd^2) * (gamma^2)), scaling_params, start = c(c = 0.03, gamma = 0.02), subset = k_used == TRUE)
 
 parameters <- tibble::tibble(
-  gamma = scale_fit[2],
+  gamma = coef(scale_fit)[2],
   b_naught = wilson_b_fit$coefficients[1],
   beta = wilson_b_fit$coefficients[2]
 )
 
+scales_points <- predictNLS(scale_fit, newdata = select(filter(scaling_params, k_used == TRUE), fwd))$summary[, c(2, 5, 6)] |> 
+  as_tibble() |> 
+  rename(pred = mean.2, lower = `2.5%`, upper = `97.5%`) |>
+  bind_cols(select(filter(scaling_params, k_used == TRUE), fwd),
+            current = _)
+
 wilson_b_plot <- ggplot2::ggplot(scaling_params, ggplot2::aes(x = fwd, y = wilson_b, color = b_used)) +
   ggplot2::geom_point() +
-  ggplot2::geom_abline(
-    slope = wilson_b_fit$coefficients[2],
-    intercept = wilson_b_fit$coefficients[1],
-    inherit.aes = FALSE,
-    linetype = 2
-  ) +
-  ggplot2::scale_color_manual(
-    "Used in Regression?",
-    labels = c("Yes", "No"),
-    breaks = c(TRUE, FALSE),
-    values = c("black", "gray80")
-  ) +
+  ggplot2::geom_smooth(data = filter(scaling_params, b_used == TRUE),
+                       method = "lm",
+                       show.legend = FALSE,
+                       linetype = 5, linewidth = 0.5) +
+  ggplot2::scale_color_manual("Point Used",
+                              labels = c("Yes", "No"),
+                              breaks = c(TRUE, FALSE),
+                              values = c("black", "gray80")) +
   ggtheme_light() +
-  ggplot2::theme(
-    legend.position = "inside",
-    legend.position.inside = c(0.95, 0.05),
-    legend.justification = c(1, 0),
-    legend.background = ggplot2::element_rect(color = "black")
-  ) +
+  ggplot2::theme(legend.position = "inside",
+                 legend.position.inside = c(0.95, 0.05),
+                 legend.justification = c(1, 0),
+                 legend.background = ggplot2::element_rect(color = "black")) +
   ggplot2::labs(
-    x = bquote(group(langle, "FWD", rangle)*" MGy"),
+    x = "Average FWD (MGy)",
     y = bquote("Wilson B-Factor ("*Å^2*")")
   )
 
 scales_plot <- ggplot2::ggplot(scaling_params, ggplot2::aes(x = fwd, y = k, color = k_used)) +
+  ggplot2::geom_ribbon(data = scales_points, 
+                       aes(x = fwd, ymin = lower, ymax = upper), 
+                       inherit.aes = FALSE, 
+                       fill = "gray", alpha = 0.5) +
+  ggplot2::geom_line(data = scales_points, 
+                     aes(x = fwd, y = pred), 
+                     inherit.aes = FALSE,
+                     color = "black", linetype = 5, linewidth = 0.5) +
   ggplot2::geom_point() +
-  ggplot2::geom_function(
-    fun = \(fwd) {scale_fit[1] * exp(-(scale_fit[2]^2) * (fwd^2))},
-    inherit.aes = FALSE,
-    linetype = 2
-  ) +
-  ggplot2::scale_color_manual(
-    "Used in Regression?",
-    labels = c("Yes", "No"),
-    breaks = c(TRUE, FALSE),
-    values = c("black", "gray80")
-  ) +
+  ggplot2::scale_color_manual("Point Used", 
+                              labels = c("Yes", "No"), 
+                              breaks = c(TRUE, FALSE),
+                              values = c("black", "gray80")) +
   ggtheme_light() +
   ggplot2::theme(
     legend.position = "inside",
@@ -78,10 +77,8 @@ scales_plot <- ggplot2::ggplot(scaling_params, ggplot2::aes(x = fwd, y = k, colo
     legend.justification = c(0, 0),
     legend.background = ggplot2::element_rect(color = "black")
   ) +
-  ggplot2::labs(
-    x = bquote(group(langle, "FWD", rangle)*" MGy"),
-    y = "Scale Factor K"
-  )
+  ggplot2::labs(x = bquote(group(langle, "FWD", rangle)*" MGy"),
+                y = "Scale Factor K")
 
 ggplot2::ggsave("2-decay_parameter_estimation/output/r_output/wilson_b_plot.svg",
                 plot = wilson_b_plot,
